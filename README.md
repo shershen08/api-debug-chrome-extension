cd7c38a94697ed8f74be61e140b5014438b90a75
mkuz <michailkuznetsov@gmail.com>
2017-10-25 23:32:12 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# SPA API data debugger extension
+
+## Usage
+
+    1. Install extension
+
+    2. Open a target tab, click Start capturing
+
+    3. Refresh the page or perform activities that leas to API calls
+
+    4. Search, filter and export obtained results.
+
+
+## ToDo
+
+ -  Caching switch, to act like a proxy when app's calls to server API are intercepted by the extension and cached data are returned
+
+
+## References
+
+    Built using VueJS and [https://github.com/cucygh/vue-chrome-extension-example](vue-chrome-extension-example)
---
+++ app/component/app.vue
@@ -0,0 +1,71 @@
+<template lang="html">
+  <div class="container">
+    <div class="columns">
+      <div class="column">
+        <div class="notification is-info">
+          <hello></hello>
+        </div>
+      </div>
+      <div class="column">
+        
+        <button v-if="!capturing">Start capturing</button>
+        <button v-if="capturing">Stop capturing</button>
+
+        <div v-if="hasResults">
+          <button>Clear cache</button>
+          <button>Export URLs</button>
+          <button>Export data JSON</button>
+        </div>
+<br>
+Results: 20<br>
+-<br>
+-<br>
+-<br>
+-<br>
+        <p class="notification is-success">
+          <label class="label">Filter by URL</label>
+          <p class="control has-icon has-icon-right">
+            <input class="input is-success" type="text" v-model="url">
+            <span class="icon is-small">
+              <i class="fa fa-check"></i>
+            </span>
+            <span class="help is-success">{{url}}</span>
+          </p>
+        </p>
+      </div>
+      <div class="column">
+        <p class="notification is-warning">Search in data</p>
+        <p class="control has-icon has-icon-right">
+            <input class="input is-success" type="text" v-model="dataStr">
+            <span class="icon is-small">
+              <i class="fa fa-check"></i>
+            </span>
+            <span class="help is-success">{{dataStr}}</span>
+          </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Hello from './hello.vue';
+export default {
+  data(){
+    return {
+      url:'*',
+      dataStr:'',
+      capturing: false,
+      hasResults: false
+    }
+  },
+  methods: {
+
+  },
+  components:{
+    hello:Hello
+  }
+}
+</script>
+
+<style lang="css">
+</style>
